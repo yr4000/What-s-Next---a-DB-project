2cc646fea3d040d6e195c6d5db185462d48d7286
Alon Itzhaki <alon.itzhaki@autodesk.com>
2017-01-02 01:41:05 +0200

    fix google maps script for all possible place types

--- taudb/whatsnext/datascripts/google_maps.py
+++ taudb/whatsnext/datascripts/google_maps.py
@@ -9,7 +9,6 @@ KEY_NEXT_PAGE_TOKEN = 'next_page_token'
 KEY_RESULTS = 'results'
 KEY_PLACE_ID = 'place_id'
 KEY_NAME = 'name'
-KEY_PRICE_LEVEL = 'price_level'
 KEY_RATING = 'rating'
 KEY_VICINITY = 'vicinity'
 KEY_GEOMETRY = 'geometry'
@@ -21,7 +20,15 @@ GOOGLE_API_KEY = 'AIzaSyCGnmhFJarg4hMWmtJF37V1NaINNXGpzBU'
 HOST = 'https://maps.googleapis.com'
 API = '/maps/api/place/search/json'
 
-conn = mdb.connect('localhost', 'root', '', 'taudb')
+# TAU DB
+conn = mdb.connect(host='127.0.0.1', port=11211, user='DbMysql06', passwd='DbMysql06', db='DbMysql06')
+
+# LOCAL DB
+# conn = mdb.connect(host='127.0.0.1', port=3306, user='root', passwd='', db='taudb')
+
+category_mapping = {'lodging': '1',
+                    'restaurant': '2',
+                    'bar': '3'}
 
 
 def run_google_maps_migration():
@@ -33,9 +40,10 @@ def run_specific_coordinates(latitude, longitude):
     print 'starting with coordinates: {lat},{long}'.format(lat=latitude, long=longitude)
 
     radius = 1000
-    place_type = 'lodging'
+    place_type = 'restaurant'
     next_page_token = None
-    rows_added = 0
+    places_added_total = 0
+    mappings_added_total = 0
 
     while True:
         print 'starting new request iteration'
@@ -67,7 +75,9 @@ def run_specific_coordinates(latitude, longitude):
 
         # write new place to db
         for place in new_places:
-            rows_added = rows_added + write_data_to_db(place)
+            places_added, mappings_added = write_data_to_db(place=place, category=place_type)
+            places_added_total = places_added_total + places_added
+            mappings_added_total = mappings_added_total + mappings_added
 
         print 'done writing data to db'
 
@@ -79,28 +89,50 @@ def run_specific_coordinates(latitude, longitude):
             print 'next page token empty - breaking out of loop'
             break
 
-    print 'done with coordinates: {lat},{long}. added {num} rows'.format(lat=latitude, long=longitude, num=rows_added)
+    print 'done with coordinates: {lat},{long}. added {num_p} places and {num_m} mappings'.format(
+        lat=latitude, long=longitude, num_p=places_added_total, num_m=mappings_added_total)
     print
 
 
-def write_data_to_db(place):
-    rows_affected = 0
+def write_data_to_db(place, category):
+    places_added = 0
+    mappings_added = 0
 
     # write data to db only if not empty
     if not place:
-        return rows_affected
+        return places_added, mappings_added
 
     cur = conn.cursor(mdb.cursors.DictCursor)
+    try:
+        # add a new place to db
+        places_added = cur.execute(
+            'insert into places (`google_id`, `name`, `rating`, `vicinity`, `latitude`, `longitude`) '
+            'values (%s, %s, %s, %s, %s, %s)',
+            (place.google_id, place.name, place.rating, place.vicinity, place.latitude, place.longitude))
 
-    rows_affected = cur.execute(
-        'insert into places (`google_id`, `name`, `rating`, `vicinity`, `latitude`, `longitude`) '
-        'values (%s, %s, %s, %s, %s, %s)',
-        (place.google_id, place.name, place.rating, place.vicinity, place.latitude, place.longitude))
+        conn.commit()
 
-    conn.commit()
+        # get the id of the newly inserted place
+        place.id = cur.lastrowid
+    except mdb.IntegrityError:
+        print 'failed to insert new place with google_id {id}. reason: already exists in places table'.format(
+            id=place.google_id)
+        # get the id of the place with this current id, to map its category later
+        cur.execute('select id from places where google_id = %s', (place.google_id,))
+        place.id = cur.fetchone()['id']
+
+    try:
+        # map the place (could be either old or new) to current category
+        mappings_added = cur.execute('insert into places_categories (`place_id`, `category_id`) values (%s, %s)',
+                                     (place.id, category_mapping[category]))
+
+        conn.commit()
+    except mdb.IntegrityError:
+        print 'failed to map place with id {id} to category {category}. reason: already exists db'.format(
+            id=place.google_id, category=category_mapping[category])
+
     cur.close()
-
-    return rows_affected
+    return places_added, mappings_added
 
 
 def convert_to_places_list(json_response):
--- taudb/whatsnext/migrations/places.sql
+++ taudb/whatsnext/migrations/places.sql
@@ -1,18 +1,51 @@
 CREATE TABLE places (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `google_id` varchar(50) UNIQUE NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `rating` float default NULL,
-  `price_level` tinyint default NULL,
-  `vicinity` varchar(255) default NULL,
-  `latitude` float(10,10) NOT NULL,
-  `longitude` float(10,10) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `idx_google_id` (`google_id`),
-  KEY `idx_name` (`name`),
-  KEY `idx_rating` (`rating`),
-  KEY `idx_price_level` (`price_level`),
-  KEY `idx_address` (`vicinity`),
-  KEY `idx_latitude` (`latitude`),
-  KEY `idx_longitude` (`longitude`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `google_id` VARCHAR(50) UNIQUE NOT NULL,
+    `name` VARCHAR(255) NOT NULL,
+    `rating` FLOAT DEFAULT NULL,
+    `vicinity` VARCHAR(255) DEFAULT NULL,
+    `latitude` FLOAT(10 , 7 ) NOT NULL,
+    `longitude` FLOAT(10 , 7 ) NOT NULL,
+    PRIMARY KEY (`id`),
+    KEY `idx_google_id` (`google_id`),
+    KEY `idx_name` (`name`),
+    KEY `idx_rating` (`rating`),
+    KEY `idx_address` (`vicinity`),
+    KEY `idx_latitude` (`latitude`),
+    KEY `idx_longitude` (`longitude`)
+)  ENGINE=INNODB DEFAULT CHARSET=UTF8;
+
+
+CREATE TABLE categories (
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(255) NOT NULL,
+    PRIMARY KEY (`id`)
+)  ENGINE=INNODB DEFAULT CHARSET=UTF8;
+
+INSERT INTO categories (`name`) VALUES ('lodging');
+INSERT INTO categories (`name`) VALUES ('restaurant');
+
+
+CREATE TABLE places_categories (
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `place_id` INT NOT NULL,
+    `category_id` INT NOT NULL,
+    PRIMARY KEY (`id`),
+    CONSTRAINT `fgn_place_id` FOREIGN KEY (`place_id`) REFERENCES places (`id`),
+    CONSTRAINT `fgn_category_id` FOREIGN KEY (`category_id`) REFERENCES categories (`id`),
+	UNIQUE KEY `place_id_category_id` (`place_id`,`category_id`)
+)  ENGINE=INNODB DEFAULT CHARSET=UTF8;
+
+
+SELECT 
+    places.id, places.google_id, categories.id, categories.name
+FROM
+    places
+        INNER JOIN
+    places_categories ON places.id = places_categories.place_id
+        INNER JOIN
+    categories ON places_categories.category_id = categories.id
+WHERE
+    places_categories.category_id=2;
+
+        
