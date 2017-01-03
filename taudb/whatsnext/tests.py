c40d5e1de8f107e0cb7b3844b4bb0a4e9f5ebe8d
yr4000 <y4000r@gmail.com>
2017-01-03 22:01:09 +0200

    quick fix in my test. sorry

--- taudb/whatsnext/tests.py
+++ taudb/whatsnext/tests.py
@@ -5,7 +5,7 @@ from views import *
 
 #TODO: every point have to be didvided by the resolution in order to use my functions. consider to cancel it
 def yair_test(request):
-    #return search_places_by_point(51.5363312*RESOLUTION,-0.0046230*RESOLUTION,0.5,"places","id, google_id")
+    #return search_places_by_point(51.5363312,-0.0046230,0.5,"places","id, google_id")
     return  pub_crawl(51.5363312,-0.0046230)
 
 pub_crawl(51.5363312,-0.0046230)
