91b062a6a8b0c7703ae9eee568d11e58adb3e742
Alon Itzhaki <alon.itzhaki@autodesk.com>
2017-01-11 00:27:33 +0200

    update the db scheme file

--- taudb/whatsnext/migrations/places.sql
+++ taudb/whatsnext/migrations/places.sql
@@ -1,19 +1,20 @@
-CREATE TABLE places (
-    `id` INT NOT NULL AUTO_INCREMENT,
-    `google_id` VARCHAR(50) UNIQUE NOT NULL,
-    `name` VARCHAR(255) NOT NULL,
-    `rating` FLOAT DEFAULT NULL,
-    `vicinity` VARCHAR(255) DEFAULT NULL,
-    `latitude` FLOAT(10 , 7 ) NOT NULL,
-    `longitude` FLOAT(10 , 7 ) NOT NULL,
-    PRIMARY KEY (`id`),
-    KEY `idx_google_id` (`google_id`),
-    KEY `idx_name` (`name`),
-    KEY `idx_rating` (`rating`),
-    KEY `idx_address` (`vicinity`),
-    KEY `idx_latitude` (`latitude`),
-    KEY `idx_longitude` (`longitude`)
-)  ENGINE=INNODB DEFAULT CHARSET=UTF8;
+-- This is the old version of places
+-- CREATE TABLE places (
+--     `id` INT NOT NULL AUTO_INCREMENT,
+--     `google_id` VARCHAR(50) UNIQUE NOT NULL,
+--     `name` VARCHAR(255) NOT NULL,
+--     `rating` FLOAT DEFAULT NULL,
+--     `vicinity` VARCHAR(255) DEFAULT NULL,
+--     `latitude` FLOAT(10 , 7 ) NOT NULL,
+--     `longitude` FLOAT(10 , 7 ) NOT NULL,
+--     PRIMARY KEY (`id`),
+--     KEY `idx_google_id` (`google_id`),
+--     KEY `idx_name` (`name`),
+--     KEY `idx_rating` (`rating`),
+--     KEY `idx_address` (`vicinity`),
+--     KEY `idx_latitude` (`latitude`),
+--     KEY `idx_longitude` (`longitude`)
+-- )  ENGINE=INNODB DEFAULT CHARSET=UTF8;
 
 
 CREATE TABLE categories (
@@ -27,16 +28,16 @@ INSERT INTO categories (`name`) VALUES ('restaurant');
 INSERT INTO categories (`name`) VALUES ('bar');
 INSERT INTO categories (`name`) VALUES ('museum');
 
-
-CREATE TABLE places_categories (
-    `id` INT NOT NULL AUTO_INCREMENT,
-    `place_id` INT NOT NULL,
-    `category_id` INT NOT NULL,
-    PRIMARY KEY (`id`),
-    CONSTRAINT `fgn_place_id` FOREIGN KEY (`place_id`) REFERENCES places (`id`),
-    CONSTRAINT `fgn_category_id` FOREIGN KEY (`category_id`) REFERENCES categories (`id`),
-	UNIQUE KEY `place_id_category_id` (`place_id`,`category_id`)
-)  ENGINE=INNODB DEFAULT CHARSET=UTF8;
+-- This is the old version of places_categories
+-- CREATE TABLE places_categories (
+--     `id` INT NOT NULL AUTO_INCREMENT,
+--     `place_id` INT NOT NULL,
+--     `category_id` INT NOT NULL,
+--     PRIMARY KEY (`id`),
+--     CONSTRAINT `fgn_place_id` FOREIGN KEY (`place_id`) REFERENCES places (`id`),
+--     CONSTRAINT `fgn_category_id` FOREIGN KEY (`category_id`) REFERENCES categories (`id`),
+-- 	UNIQUE KEY `place_id_category_id` (`place_id`,`category_id`)
+-- )  ENGINE=INNODB DEFAULT CHARSET=UTF8;
 
 
 CREATE TABLE reviews (
@@ -78,7 +79,7 @@ CREATE TABLE places_categories_v2 (
     CONSTRAINT `fgn_pc2_place_id` FOREIGN KEY (`place_id`) REFERENCES places (`id`),
     CONSTRAINT `fgn_pc2_category_id` FOREIGN KEY (`category_id`) REFERENCES categories (`id`),
 	UNIQUE KEY `place_id_category_id` (`place_id`,`category_id`)
-)  ENGINE=INNODB DEFAULT CHARSET=UTF8;
+)  ENGINE=MyISAM DEFAULT CHARSET=UTF8;
 
 
 CREATE TABLE search_popularity
