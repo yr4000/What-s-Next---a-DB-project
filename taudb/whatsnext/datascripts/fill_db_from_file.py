b152a6c1ebebc36b1d06cdf3a1fbc4021be378b8
Alon Itzhaki <alon.itzhaki@autodesk.com>
2017-01-11 00:26:46 +0200

    comment out the dangerous db reconstruction script

--- taudb/whatsnext/datascripts/fill_db_from_file.py
+++ taudb/whatsnext/datascripts/fill_db_from_file.py
@@ -2,68 +2,77 @@ import csv
 
 import MySQLdb as mdb
 
+'''
+                                !!!!! IMPORTANT !!!!!
 
-def insert_places_to_table():
-    conn = mdb.connect(host='127.0.0.1', user='DbMysql06', passwd='DbMysql06', db='DbMysql06', port=11211)
-    cur = conn.cursor(mdb.cursors.DictCursor)
+                DO NOT RUN THIS FILE UNLESS YOUR NAME IS ALON ITZHAKI!!!!!!
+                        EVERYTHING IS IN COMMENT AS A PRECAUTION
 
-    with open('places_05_01_17_converted.csv', 'rb') as f:
-        reader = csv.reader(f)
-        i = 1
-        tuples_to_add = list()
-        for id, google_id, name, rating, vicinity, latitude, longitude in reader:
-            tuples_to_add.append((id, google_id, name, rating, vicinity, latitude, longitude))
-
-            if i == 1000:
-                # insert 1000 tuples at a time
-                cur.executemany(
-                    'INSERT INTO places_v2 (`id`, `google_id`, `name`, `rating`, `vicinity`, `latitude`, `longitude`) '
-                    ' VALUES (%s, %s, %s, %s, %s, %s, %s)', tuples_to_add)
-                i = 1
-                conn.commit()
-                print 'inserted {num} tuples to places table'.format(num=len(tuples_to_add))
-                tuples_to_add = list()
-            i += 1
-
-        # leftovers run for the last tuples that were aggregated
-        cur.executemany(
-            'INSERT INTO places_v2 (`id`, `google_id`, `name`, `rating`, `vicinity`, `latitude`, `longitude`) '
-            ' VALUES (%s, %s, %s, %s, %s, %s, %s)', tuples_to_add)
-        conn.commit()
-        print 'inserted {num} tuples to places table'.format(num=len(tuples_to_add))
-    cur.close()
+                                !!!!! IMPORTANT !!!!!
+'''
 
 
-def insert_places_categories_to_table():
-    conn = mdb.connect(host='127.0.0.1', user='DbMysql06', passwd='DbMysql06', db='DbMysql06', port=11211)
-    cur = conn.cursor(mdb.cursors.DictCursor)
+# def insert_places_to_table():
+#     conn = mdb.connect(host='127.0.0.1', user='DbMysql06', passwd='DbMysql06', db='DbMysql06', port=3305)
+#     cur = conn.cursor(mdb.cursors.DictCursor)
+#
+#     with open('places_05_01_17_converted.csv', 'rb') as f:
+#         reader = csv.reader(f)
+#         i = 1
+#         tuples_to_add = list()
+#         for id, google_id, name, rating, vicinity, latitude, longitude in reader:
+#             tuples_to_add.append((id, google_id, name, rating, vicinity, latitude, longitude))
+#
+#             if i == 1000:
+#                 # insert 1000 tuples at a time
+#                 cur.executemany(
+#                     'INSERT INTO places (`id`, `google_id`, `name`, `rating`, `vicinity`, `latitude`, `longitude`) '
+#                     ' VALUES (%s, %s, %s, %s, %s, %s, %s)', tuples_to_add)
+#                 i = 1
+#                 conn.commit()
+#                 print 'inserted {num} tuples to places table'.format(num=len(tuples_to_add))
+#                 tuples_to_add = list()
+#             i += 1
+#
+#         # leftovers run for the last tuples that were aggregated
+#         cur.executemany(
+#             'INSERT INTO places (`id`, `google_id`, `name`, `rating`, `vicinity`, `latitude`, `longitude`) '
+#             ' VALUES (%s, %s, %s, %s, %s, %s, %s)', tuples_to_add)
+#         conn.commit()
+#         print 'inserted {num} tuples to places table'.format(num=len(tuples_to_add))
+#     cur.close()
 
-    with open('places_categories_05_01_17.csv', 'rb') as f:
-        reader = csv.reader(f)
-        i = 1
-        tuples_to_add = list()
-        for id, place_id, category_id in reader:
-            tuples_to_add.append((id, place_id, category_id))
 
-            if i == 1000:
-                # insert 1000 tuples at a time
-                cur.executemany(
-                    'INSERT INTO places_categories_v2 (`id`, `place_id`, `category_id`) '
-                    ' VALUES (%s, %s, %s)', tuples_to_add)
-                i = 1
-                conn.commit()
-                print 'inserted {num} tuples to places_categories table'.format(num=len(tuples_to_add))
-                tuples_to_add = list()
-            i += 1
-
-        # leftovers run for the last tuples that were aggregated
-        cur.executemany(
-            'INSERT INTO places_categories_v2 (`id`, `place_id`, `category_id`) '
-            ' VALUES (%s, %s, %s)', tuples_to_add)
-        conn.commit()
-        print 'inserted {num} tuples to places_categories table'.format(num=len(tuples_to_add))
-    cur.close()
+# def insert_places_categories_to_table():
+#     conn = mdb.connect(host='127.0.0.1', user='DbMysql06', passwd='DbMysql06', db='DbMysql06', port=3305)
+#     cur = conn.cursor(mdb.cursors.DictCursor)
+#
+#     with open('places_categories_05_01_17.csv', 'rb') as f:
+#         reader = csv.reader(f)
+#         i = 1
+#         tuples_to_add = list()
+#         for id, place_id, category_id in reader:
+#             tuples_to_add.append((id, place_id, category_id))
+#
+#             if i == 1000:
+#                 # insert 1000 tuples at a time
+#                 cur.executemany(
+#                     'INSERT INTO places_categories (`id`, `place_id`, `category_id`) '
+#                     ' VALUES (%s, %s, %s)', tuples_to_add)
+#                 i = 1
+#                 conn.commit()
+#                 print 'inserted {num} tuples to places_categories table'.format(num=len(tuples_to_add))
+#                 tuples_to_add = list()
+#             i += 1
+#
+#         # leftovers run for the last tuples that were aggregated
+#         cur.executemany(
+#             'INSERT INTO places_categories (`id`, `place_id`, `category_id`) '
+#             ' VALUES (%s, %s, %s)', tuples_to_add)
+#         conn.commit()
+#         print 'inserted {num} tuples to places_categories table'.format(num=len(tuples_to_add))
+#     cur.close()
 
 
 # insert_places_to_table()
-insert_places_categories_to_table()
+# insert_places_categories_to_table()
