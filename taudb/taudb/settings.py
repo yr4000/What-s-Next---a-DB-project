9ea5b621eba605033ab17406c5a3ef72059d1c41
Alon Mey Tal <alonm@pmax.co>
2017-01-17 22:07:04 +0200

    Run in DEBUG mode only in windows/mac os devices (and not Linux = deployment)

--- taudb/taudb/settings.py
+++ taudb/taudb/settings.py
@@ -23,9 +23,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'zv%8hm)_jzd@%ph2$3l5&__r0%tyu@x*_t_-wp2d%(xspu$qsx'
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
 ALLOWED_HOSTS = []
 
 
@@ -119,9 +116,11 @@ DB_CHARSET = 'utf8'
 if platform.system() == 'Linux':
     DB_HOST = "mysqlsrv.cs.tau.ac.il"
     DB_PORT = 3306
+    DEBUG = False
 elif platform.system() == 'Windows' or platform.system() == 'Darwin':
     DB_HOST = "127.0.0.1"
     DB_PORT = 3305
+    DEBUG = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
